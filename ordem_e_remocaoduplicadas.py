3d63733778175ce0c0a0eb37a191211f228181bc
Satiro07 <jardelsatiro57@gmail.com>
2025-04-21 22:32:35 -0300

    Update ordem_e_remocaoduplicadas.py

--- ordem_e_remocaoduplicadas.py
+++ ordem_e_remocaoduplicadas.py
@@ -1,9 +1,13 @@
 def remover_duplicados(lista):
     lista_atualizada = []
     for i in lista:
-        if i.lower() not in lista_atualizada:
-            lista_atualizada.append(i.lower())
-    return f'Lista atulizada, sem duplicados e em ordem alfabética: {sorted(lista_atualizada)}'
+        verificacao = False
+        for y in lista_atualizada:
+            if i.lower() == y.lower():
+                verificacao = True
+        if verificacao == False:
+            lista_atualizada.append(i)
+    return f'Lista atualizada, sem duplicados e em ordem alfabética: {sorted(lista_atualizada)}'
 
 lista = ['José', 'Joao', 'R. Mácario Pontes', 'José', 'joao']
 print(remover_duplicados(lista))
