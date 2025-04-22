bfd9c9e35ff0538b012182a169ef419ef6f9358d
Satiro07 <jardelsatiro57@gmail.com>
2025-04-21 22:18:55 -0300

    Update somar_numeros.py

--- somar_numeros.py
+++ somar_numeros.py
@@ -1,8 +1,10 @@
+def verificar_lista(lista):
+    somente_numeros = []
+    for i in lista:
+        verificacao = str(i).isnumeric()
+        if verificacao == True:
+            somente_numeros.append(i)
+    soma_numeros = sum(somente_numeros)
+    return f'A soma dos números que estavam na lista é: {soma_numeros}'
 lista = [1,2, 5, 66, 'oi9', 'oi', 0, '0o']
-somente_numeros = []
-for i in lista:
-    verificacao = str(i).isnumeric()
-    if verificacao == True:
-        somente_numeros.append(i)
-soma_numeros = sum(somente_numeros)
-print(f'A soma dos números que estavam na lista é: {soma_numeros}')
+print(verificar_lista(lista))
